b6b553291fcaa94b799ed9d0dc218675f1a368f2
Shams Kabir <kabir.shams.2002@gmail.com>
2026-06-23 01:28:17 +0600

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -109,7 +109,7 @@ async def generate_languages(s: Stats) -> None:
             lang_list += f'<div class="lang-row" style="display: flex; gap: 16px; margin-bottom: 6px; animation-delay: {i * delay_between}ms;">\n'
 
         lang_list += f"""\
-  <div class="lang-item" style="display: flex; align-items: center; gap: 7px; flex: 1; white-space: nowrap; font-size: 16px;">
+  <div class="lang-item" style="display: flex; align-items: center; gap: 7px; flex: 1; white-space: nowrap; font-size: 17px;">
     <svg xmlns="http://www.w3.org/2000/svg" class="octicon" style="fill:{color}; opacity: 0.9; flex-shrink: 0;"
       viewBox="0 0 16 16" version="1.1" width="16" height="16">
       <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path>
